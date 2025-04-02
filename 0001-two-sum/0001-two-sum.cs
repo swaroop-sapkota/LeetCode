7df4fe665c89a38a2aba7a09b06f681950b8b30c
Swaroop Sapkota <swaroopsapkota1@gmail.com>
2025-04-02 10:30:12 -0500

    Time: 1 ms (98.14%), Space: 49.2 MB (16.79%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        Dictionary<int, int> dict = new Dictionary<int,int>();
+
+        for(int i = 0; i < nums.Length; i++)
+        {
+            int diff = target - nums[i];
+            if (dict.ContainsKey(diff))
+            {
+                return new int[] {dict[diff], i};
+            }
+            dict[nums[i]] = i;
+        }
+        return new int[] {};
+    }
+}
+
+        // Time complexity: O(n)
