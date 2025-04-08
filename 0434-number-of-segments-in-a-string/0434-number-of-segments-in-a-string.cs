c0679a87b0f74a897eec05618dfcc55cb177dc29
Swaroop Sapkota <swaroopsapkota1@gmail.com>
2025-04-07 22:13:42 -0500

    Time: 0 ms (100%), Space: 37.7 MB (100%) - LeetHub

--- 0434-number-of-segments-in-a-string/0434-number-of-segments-in-a-string.cs
+++ 0434-number-of-segments-in-a-string/0434-number-of-segments-in-a-string.cs
@@ -1,16 +1,23 @@
 public class Solution {
     public int CountSegments(string s) {
-        string[] words = s.Split(' ');
-
         int count = 0;
+        bool inSegment = false;  // To track if we're inside a word
 
-        foreach (string word in words)
-        {
-            if(word.Length > 0)
-            {
-                count++;
+        // Step through each character in the string
+        for (int i = 0; i < s.Length; i++) {
+            // If we encounter a non-space character
+            if (s[i] != ' ') {
+                // If we are not already inside a word, this is the start of a new word
+                if (!inSegment) {
+                    count++;
+                    inSegment = true;  // We are now inside a word
+                }
+            } else {
+                // If we encounter a space, we are no longer inside a word
+                inSegment = false;
             }
         }
-        return count;
+
+        return count;  // Return the total number of words (segments)
     }
 }
