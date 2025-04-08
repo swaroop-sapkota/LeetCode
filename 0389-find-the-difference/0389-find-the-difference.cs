a9e98560557eea33c58bc6b1173557a6a6cb4d39
Swaroop Sapkota <swaroopsapkota1@gmail.com>
2025-04-07 21:35:35 -0500

    Time: 2 ms (56.55%), Space: 42.6 MB (56.01%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cs
+++ 0389-find-the-difference/0389-find-the-difference.cs
@@ -1,18 +1,21 @@
 public class Solution {
     public char FindTheDifference(string s, string t) {
         
-        int sumS = 0, sumT = 0;
-
-        foreach (char c in s)
-        {
-            sumS += c;
-        }
+        List<char> sList = new List<char>(s);
 
         foreach(char c in t)
         {
-            sumT += c;
+            if(sList.Contains(c))
+            {
+                sList.Remove(c);
+            }
+
+            else
+            {
+                return c;
+            }
         }
 
-        return (char)(sumT - sumS);
+        return '\0';
 }
 }
