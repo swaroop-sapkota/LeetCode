17917ce159e3edec90ea2c863f7fd1468ce88ec8
Swaroop Sapkota <swaroopsapkota1@gmail.com>
2025-04-07 21:42:08 -0500

    Time: 65 ms (58.38%), Space: 41.6 MB (7.3%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.cs
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.cs
@@ -1,21 +1,25 @@
 public class Solution {
     public string MergeAlternately(string word1, string word2) {
-        Queue<char> queue = new Queue<char>();
+        //Queue<char> queue = new Queue<char>();
         int p = 0, q = 0;
+        string result = "";
         
         while (p < word1.Length || q < word2.Length) 
         {
             if (p < word1.Length) 
             {
-                queue.Enqueue(word1[p]);
+                //queue.Enqueue(word1[p]);
+                result += word1[p];
                 p++;
             }
             if (q < word2.Length) {
-                queue.Enqueue(word2[q]);
+                //queue.Enqueue(word2[q]);
+                result += word2[q];
                 q++;
             }
         }
         
-        return new string(queue.ToArray());
+        return result;
+        //return new string(queue.ToArray());
     }
 }
