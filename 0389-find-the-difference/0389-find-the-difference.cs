aabf3388470e08e0b76f4219f38628f8bb40d1e0
Swaroop Sapkota <swaroopsapkota1@gmail.com>
2025-04-07 21:36:32 -0500

    Time: 1 ms (74.51%), Space: 42.7 MB (43.09%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cs
+++ 0389-find-the-difference/0389-find-the-difference.cs
@@ -1,6 +1,22 @@
 public class Solution {
     public char FindTheDifference(string s, string t) {
         
+        int sumS = 0, sumT = 0;
+
+        foreach (char c in s)
+        {
+            sumS += c;
+        }
+
+        foreach(char c in t)
+        {
+            sumT += c;
+        }
+
+        return (char)(sumT - sumS);
+}
+}
+/*
         List<char> sList = new List<char>(s);
 
         foreach(char c in t)
@@ -17,5 +33,4 @@ public class Solution {
         }
 
         return '\0';
-}
-}
+*/
