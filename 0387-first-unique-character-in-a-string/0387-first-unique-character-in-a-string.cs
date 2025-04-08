fa60933cda22f400d47b45d689a6d3005f07a81b
Swaroop Sapkota <swaroopsapkota1@gmail.com>
2025-04-07 21:52:18 -0500

    Time: 10 ms (80.87%), Space: 48.8 MB (74.61%) - LeetHub

---
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cs
@@ -0,0 +1,24 @@
+public class Solution {
+    public int FirstUniqChar(string s) {
+
+       for(int i = 0; i < s.Length; i++)
+       {
+        bool isUniq = true;
+        for(int j = 0; j < s.Length; j++)
+        {
+            if(i!=j && s[i] == s[j])
+            {
+                isUniq = false;
+                break;
+            }
+        }
+
+        if(isUniq)
+        {
+            return i;
+        }
+       }
+
+       return -1; 
+    }
+}
