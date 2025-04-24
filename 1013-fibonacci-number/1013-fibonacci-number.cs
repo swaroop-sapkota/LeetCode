b302e6d67a8ec5263388c7fea0a68095047c5158
Swaroop Sapkota <swaroopsapkota1@gmail.com>
2025-04-24 17:31:31 -0500

    Time: 23 ms (79.46%), Space: 29 MB (35.13%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cs
+++ 1013-fibonacci-number/1013-fibonacci-number.cs
@@ -1,16 +1,15 @@
 public class Solution {
     public int Fib(int n) {
         
-        if(n == 0) return 0;
+        if(n==0) return 0;
 
-        int f0 = 0, f1 = 1;
-        int f = f0 + f1;
-
-        for(int i = 2; i < n; i++)
+        int f0 = 0,f1 = 1;
+        var f= f0 + f1;        
+        for(var i = 2; i < n; i++)
         {
             f0 = f1;
             f1 = f;
-            f = f0 + f1;
+            f= f0 + f1;
         }
 
         return f;
