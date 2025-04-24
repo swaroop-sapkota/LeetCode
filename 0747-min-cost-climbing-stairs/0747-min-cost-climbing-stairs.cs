c770bc42fc9240a9ac48bf7c6dff7eee6d95def9
Swaroop Sapkota <swaroopsapkota1@gmail.com>
2025-04-24 18:33:28 -0500

    Time: 0 ms (100%), Space: 43.3 MB (55.58%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cs
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cs
@@ -2,29 +2,29 @@ public class Solution {
     public int MinCostClimbingStairs(int[] cost) {
         int n = cost.Length;
 
-        int[] dp = new int[n + 1];
+        int[] minCost = new int[n + 1];
 
-        dp[0] = 0;
-        dp[1] = 0; 
+        minCost[0] = 0;
+        minCost[1] = 0; 
 
         for(int i = 2; i <= n; i++)
         {
-            dp[i] = Math.Min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
+            minCost[i] = Math.Min(minCost[i - 1] + cost[i - 1], minCost[i - 2] + cost[i - 2]);
 
             /*
-            int one = dp[i - 1] + cost[i - 1];
-            int two = dp[i - 2] + cost[i - 2];
+            int one = minCost[i - 1] + cost[i - 1];
+            int two = minCost[i - 2] + cost[i - 2];
             if(one < two)
             {
-                dp[i] = one;
+                minCost[i] = one;
             }
             else
             {
-                dp[i] = two;
+                minCost[i] = two;
             }
             */
         }
 
-        return dp[n];
+        return minCost[n];
     }
 }
