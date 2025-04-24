56995c5e409dcffb64a027c0684273ba86d5a51a
Swaroop Sapkota <swaroopsapkota1@gmail.com>
2025-04-24 16:19:52 -0500

    Time: 0 ms (100%), Space: 49.3 MB (94.57%) - LeetHub

--- 0169-majority-element/0169-majority-element.cs
+++ 0169-majority-element/0169-majority-element.cs
@@ -20,22 +20,5 @@ public class Solution {
         }
         return majority;
 
-        // By Sorting
-        /*
-        for (int i = 0; i < nums.Length - 1; i++)
-        {
-            for (int j = 0; j < nums.Length - i - 1; j++)
-            {
-                if (nums[j] > nums[j + 1])
-                {
-                    int temp = nums[j];
-                    nums[j] = nums[j + 1];
-                    nums[j + 1] = temp;
-                }
-            }
-        }
-        return nums[nums.Length / 2];
-        */
-
     }
 }
