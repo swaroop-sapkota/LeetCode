072c6985182dabc436c9c97ad5806cbd182ef307
Swaroop Sapkota <swaroopsapkota1@gmail.com>
2025-04-24 16:19:21 -0500

    Time: 0 ms (100%), Space: 64.3 MB (98.09%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cs
+++ 0344-reverse-string/0344-reverse-string.cs
@@ -1,8 +1,6 @@
 public class Solution {
     public void ReverseString(char[] s) {
         
-        //char[] reversed = new char[]{};
-
         int j = s.Length - 1;
         
         for(int i = 0; i < j; i++, j--)
