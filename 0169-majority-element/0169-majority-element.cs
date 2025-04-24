2377655d27300b69230d8d21f865fdcd171a527c
Swaroop Sapkota <swaroopsapkota1@gmail.com>
2025-04-24 12:47:33 -0500

    Time: 0 ms (100%), Space: 49.7 MB (48.68%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cs
@@ -0,0 +1,37 @@
+public class Solution {
+    public int MajorityElement(int[] nums) {
+        
+        // Moores voting algorithm
+
+        int majority = nums[0], votes = 1;
+
+        for(int i = 1; i < nums.Length; i++)
+        {
+            if(nums[i] == majority) votes++;
+            else if(votes == 0)
+            {
+                votes++;
+                majority = nums[i];
+            }
+            else
+            {
+                votes--;
+            }
+        }
+        return majority;
+
+        // By Sorting
+        /*
+        for (int i = 0; i < nums.Length - 1; i++)
+        {
+            for (int j = 0; j < nums.Length - i - 1; j++)
+            {
+                int temp = nums[j];
+                nums[j] = nums[j + 1];
+                nums[j + 1] = temp;
+            }
+        }
+        return nums[nums.Length / 2];
+        */
+    }
+}
