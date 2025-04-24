15f97bfddf082dc20479ead000e4a55dbf8445b5
Swaroop Sapkota <swaroopsapkota1@gmail.com>
2025-04-24 17:30:30 -0500

    Time: 27 ms (58.14%), Space: 28.7 MB (82.15%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cs
+++ 1013-fibonacci-number/1013-fibonacci-number.cs
@@ -1,24 +1,18 @@
 public class Solution {
     public int Fib(int n) {
-        if(n <= 1)
+        
+        if(n == 0) return 0;
+
+        int f0 = 0, f1 = 1;
+        int f = f0 + f1;
+
+        for(int i = 2; i < n; i++)
         {
-            return n;
+            f0 = f1;
+            f1 = f;
+            f = f0 + f1;
         }
 
-        // initialize our memoization map
-        // size N+1 so that we can accommodate from 0 to N
-        int[] dp = new int[n + 1];
-
-        // put our base case
-        dp[0] = 0;
-        dp[1] = 1;
-
-        //iterate through remaining values (2....N)
-        for( int i = 2; i <= n; i++)
-        {
-            dp[i] = dp[i - 1] + dp[i - 2];
-        }
-
-        return dp[n];
+        return f;
     }
 }
