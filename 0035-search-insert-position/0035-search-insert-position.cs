a23aa5d40552d4712be20d86c072d9d777e57e8c
Swaroop Sapkota <swaroopsapkota1@gmail.com>
2025-04-24 19:21:46 -0500

    Time: 0 ms (100%), Space: 43.6 MB (7.04%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public int SearchInsert(int[] nums, int target) {
+        
+        for(int i = 0; i < nums.Length; i++)
+        {
+            if(nums[i] == target)
+            {
+                return i;
+            }
+            else if(nums[i] > target)
+            {
+                return i;
+            }
+        }
+        return nums.Length;
+    }
+}
