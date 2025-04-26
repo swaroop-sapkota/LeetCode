ff3ad7eff1aa4ae6ef5b5c8f86a63c4656d75660
Swaroop Sapkota <swaroopsapkota1@gmail.com>
2025-04-25 23:47:43 -0500

    Time: 5 ms (72.99%), Space: 43.7 MB (50.27%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cs
+++ 0242-valid-anagram/0242-valid-anagram.cs
@@ -1,29 +1,36 @@
 public class Solution {
     public bool IsAnagram(string s, string t) {
-
-        if (s.Length != t.Length) return false;
+        
+        if (s.Length != t.Length) {
+            return false;
+        }
 
         Dictionary<char, int> letterCounts = new Dictionary<char, int>();
 
-        foreach (char letter in s) {
+        for (int i = 0; i < s.Length; i++) {
+            char letter = s[i];
+
             if (letterCounts.ContainsKey(letter)) {
-                letterCounts[letter]++;
+                letterCounts[letter] = letterCounts[letter] + 1;
             } else {
                 letterCounts[letter] = 1;
             }
         }
 
-        foreach (char letter in t) {
+        for (int i = 0; i < t.Length; i++) {
+            char letter = t[i];
+
             if (!letterCounts.ContainsKey(letter)) {
                 return false;
             }
 
-            letterCounts[letter]--;
+            letterCounts[letter] = letterCounts[letter] - 1;
 
             if (letterCounts[letter] < 0) {
                 return false;
             }
         }
+
         return true;
     }
 }
