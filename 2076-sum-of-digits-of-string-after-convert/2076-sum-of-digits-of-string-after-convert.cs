04abb23e71f9f54726f67203df296d411e485bc1
Swaroop Sapkota <swaroopsapkota1@gmail.com>
2025-04-28 16:30:00 -0500

    Time: 0 ms (100%), Space: 39.8 MB (49.33%) - LeetHub

---
+++ 2076-sum-of-digits-of-string-after-convert/2076-sum-of-digits-of-string-after-convert.cs
@@ -0,0 +1,36 @@
+public class Solution {
+    public int GetLucky(string s, int k) {
+
+        int sum = 0;
+
+        //string st = "";
+
+        foreach(char c in s)
+        {
+            int num = c - 'a' + 1;
+
+            while(num > 0)
+            {
+                sum += (num % 10);
+                num /= 10;
+            }
+        }
+
+        k--;
+
+        while(k > 0)
+        {
+            int total = 0;
+
+            foreach(char digit in sum.ToString())
+            {
+                total += digit - '0';
+            }
+            sum = total;
+            k--;
+        }
+
+        return sum;
+
+    }
+}
