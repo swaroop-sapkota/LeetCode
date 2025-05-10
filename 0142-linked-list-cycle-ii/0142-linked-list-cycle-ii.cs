4740f601152f73f40569de42fcfe04d0ec5d9a0c
Swaroop Sapkota <swaroopsapkota1@gmail.com>
2025-05-09 23:35:24 -0500

    Time: 80 ms (95.53%), Space: 45.6 MB (35.98%) - LeetHub

---
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cs
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode DetectCycle(ListNode head) {
+        ListNode slow = head, fast = head;
+
+        while(fast != null && fast.next != null)
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if(slow == fast)
+            {
+                while (head != slow)
+                {
+                    head = head.next;
+                    slow = slow.next;
+                }
+                return slow;
+            }
+        }
+
+        return null;
+    }
+}
