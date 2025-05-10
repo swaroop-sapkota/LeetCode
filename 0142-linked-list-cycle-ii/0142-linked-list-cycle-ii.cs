ea2d30776183da3bcc30077036d2d9197473c73f
Swaroop Sapkota <swaroopsapkota1@gmail.com>
2025-05-09 23:35:49 -0500

    Time: 84 ms (91.06%), Space: 45.9 MB (25.81%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cs
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cs
@@ -25,7 +25,7 @@ public class Solution {
                     head = head.next;
                     slow = slow.next;
                 }
-                return head;
+                return slow;
             }
         }
 
