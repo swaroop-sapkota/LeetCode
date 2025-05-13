6759cdad0135c89a19b5a50251812699738dbcc3
Swaroop Sapkota <swaroopsapkota1@gmail.com>
2025-05-12 22:05:11 -0500

    Time: 27 ms (47.98%), Space: 28.8 MB (88.37%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cs
+++ 0007-reverse-integer/0007-reverse-integer.cs
@@ -1,17 +1,18 @@
 public class Solution {
     public int Reverse(int x) {
-        
         int rev = 0;
 
-        while(x != 0)
-        {
+        while (x != 0) {
             int rem = x % 10;
 
-            if(rev > int.MaxValue / 10 || rev < int.MinValue / 10)
-            {
+            if (rev > int.MaxValue / 10 || (rev == int.MaxValue / 10 && rem > 7)) {
                 return 0;
             }
-            rev = (rev * 10) + rem;
+            if (rev < int.MinValue / 10 || (rev == int.MinValue / 10 && rem < -8)) {
+                return 0;
+            }
+
+            rev = rev * 10 + rem;
             x /= 10;
         }
 
