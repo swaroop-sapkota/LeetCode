83bd8df2b2dc6ba061cf8cfd072ade5390993e4a
Swaroop Sapkota <swaroopsapkota1@gmail.com>
2025-05-12 22:02:14 -0500

    Time: 30 ms (26.22%), Space: 28.7 MB (88.37%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cs
@@ -0,0 +1,20 @@
+public class Solution {
+    public int Reverse(int x) {
+        
+        int rev = 0;
+
+        while(x != 0)
+        {
+            int rem = x % 10;
+
+            if(rev > int.MaxValue / 10 || rev < int.MinValue / 10)
+            {
+                return 0;
+            }
+            rev = (rev * 10) + rem;
+            x /= 10;
+        }
+
+        return rev;
+    }
+}
